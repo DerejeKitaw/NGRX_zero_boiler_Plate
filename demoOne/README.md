d2a55d324294205518bee52afaaf4c9e00980772
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-11 22:50:08 -0400

    heroes components gernerated

--- demoOne/README.md
+++ demoOne/README.md
@@ -8,3 +8,14 @@ ng new demoOne --routing -sg -st --style=scss -d
 ```
 ng g module heores --routing -d
 ```
+### generate heroes service
+open  the terminal in heroes folder and run
+```
+ng g service heores -d
+```
+### generate heroes, hero-list and hero-detail component
+```
+ng g c heroes -d
+ng g c hero-list -d
+ng g c hero-detail -d
+```
--- demoOne/src/app/heores/heores.module.ts
+++ demoOne/src/app/heores/heores.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeoresRoutingModule } from './heores-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HeoresRoutingModule
   ],
-  declarations: []
+  declarations: [HeroesComponent, HeroListComponent, HeroDetailComponent]
 })
 export class HeoresModule { }
