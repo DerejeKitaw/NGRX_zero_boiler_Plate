64807d875a95e14a9b318afb435bc94201dd471d
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-11 22:39:05 -0400

    heroes module generated

---
+++ demoOne/src/app/heores/heores.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeoresRoutingModule } from './heores-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HeoresRoutingModule
+  ],
+  declarations: []
+})
+export class HeoresModule { }
